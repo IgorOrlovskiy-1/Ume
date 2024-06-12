c11ebc844e527a743a46afc09715b1f1c6bce89e
Semyon Zernov <st095228@student.spbu.ru>
2024-06-12 15:02:25 +0300

    Air + Echo

--- cmd/ume/main.go
+++ cmd/ume/main.go
@@ -3,20 +3,36 @@ package main
 import (
 	"Ume/internal/config"
 	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
 )
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
 
 func main() {
 	cfg := config.MustLoad()
-
 	fmt.Println(cfg)
 
-	//TODO: logger
+	logger := middleware.Logger()
 
 	//TODO: bd
 
-	//TODO: router
+	// Server instance
+	e := echo.New()
 
-	//TODO: middlewars
+	// Middleware
+	e.Use(logger)
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1024"))
 
 	//TODO: tests
 }

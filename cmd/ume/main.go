acf71fa03545310bad61f3cc853634f34ca82254
Semyon Zernov <st095228@student.spbu.ru>
2024-06-13 11:08:03 +0300

    Added small login page + redirect to /chats

--- cmd/ume/main.go
+++ cmd/ume/main.go
@@ -5,12 +5,13 @@ import (
 	"Ume/internal/config"
 	"Ume/internal/lib/logger/sl"
 	"Ume/internal/storage/postgresql"
+	"net/http"
 
-	"context"
+	"fmt"
 	"log/slog"
 	"os"
 
-	_ "github.com/a-h/templ"
+	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -21,17 +22,51 @@ const (
 	envProd  = "prod"
 )
 
-// Handler
-func nullpage(c echo.Context) error {
-	data := components.Hello("World").Render(context.Background(), c.Response().Writer)
-	return data
+var cfg = config.MustLoad()
+var log = setupLogger(cfg.Env)
+
+func CheckUserCreds(login string, password string) (error, bool) {
+	// TODO
+	if login == "123" && password == "123" {
+		return nil, true
+	}
+	return nil, false
+}
+
+func Login(c echo.Context) error {
+	login := c.FormValue("login")
+	password := c.FormValue("password")
+	err, status := CheckUserCreds(login, password)
+	if err != nil {
+		log.Info("Failed to login", sl.Err(err))
+	}
+	fmt.Printf("Status:%t Login:%s  Password:%s\n", status, login, password)
+	if !status {
+        return Render(c, http.StatusOK, components.BadLogin())
+	}
+	return Render(c, http.StatusOK, components.GoodLogin())
+}
+
+func HomeHandler(c echo.Context) error {
+	return Render(c, http.StatusOK, components.Home())
+}
+
+func ChatHandler(c echo.Context) error {
+	return Render(c, http.StatusOK, components.Chat())
+}
+
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	buf := templ.GetBuffer()
+	defer templ.ReleaseBuffer(buf)
+
+	if err := t.Render(ctx.Request().Context(), buf); err != nil {
+		return err
+	}
+
+	return ctx.HTML(statusCode, buf.String())
 }
 
 func main() {
-	cfg := config.MustLoad()
-
-	log := setupLogger(cfg.Env)
-
 	db, err := postgresql.New(
 		"user=" + cfg.User + "password=" + cfg.Password + "dbname=" + cfg.DBName + "ssl=" + cfg.SSLMode,
 	)
@@ -49,12 +84,19 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Routes
-	e.GET("/", nullpage)
-
+	e.Static("/assets", "components/assets")
+	e.GET("/", HomeHandler)
+	e.GET("/chat", ChatHandler)
+	e.POST("/login", Login)
 	// Start server
-	e.Logger.Fatal(e.Start(":" + cfg.Port))
-
+	s := http.Server{
+		Addr:        ":" + cfg.Port,
+		Handler:     e,
+		ReadTimeout: cfg.Timeout,
+	}
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
+		e.Logger.Fatal(err)
+	}
 	//TODO: tests
 }
 
